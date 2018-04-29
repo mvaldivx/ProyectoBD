51d2dfcfdec28f463cf3f450454ce35e0fc2f2f2
Mauricio Valdivia <mauriciovaldivia@MacBook-Air-de-Mauricio.local>
2018-04-29 15:17:43 -0500

    Inserta Materia, Alumnos, Maestros, Aulas

--- src/app/principal-admin/principal-admin.component.ts
+++ src/app/principal-admin/principal-admin.component.ts
@@ -11,11 +11,27 @@ import {  Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./principal-admin.component.css']
 })
 export class PrincipalAdminComponent implements OnInit {
-
+  modalRef:any;
   materiaForm: FormGroup;
-  codigoDB:any;
+
+  codigoMateria = 0;
+  codigoAlumno = 0;
+  codigoAula = 0;
+  codigoMaestro = 0;
   usuario:any;
-  materia = {codigo : this.codigoDB,nombre: '',carrera:'', departamento: '', horas: '', creditos: '', turno: '', LI:'',LF:'',MI:'',MF:'',MiI:'',MiF:'',JI:'',JF:'',VI:'',VF:'',SI:'',SF:''};
+  AulaMaestro:any[];
+  Aulas: any[];
+  Carreras: any[];
+  tdStyle= [];
+  tdMateria=[];
+  MateriaCorrelacion=[];
+  tablaHorarios="tablaDefault";
+ 
+
+  Maestro = {codigo: this.codigoMaestro};
+  Aula = {codigo: this.codigoAula};
+  Alumno = {codigo: this.codigoAlumno};
+  materia = {codigo : this.codigoAlumno,nombre: '',carrera:'', departamento: '', horas: '', creditos: '', turno: ''};
 
   constructor(
     private modalService:NgbModal,
@@ -35,20 +51,32 @@ export class PrincipalAdminComponent implements OnInit {
       'dep': new FormControl(this.materia.departamento, [Validators.required]),
       'horas': new FormControl(this.materia.horas, Validators.required),
       'creditos': new FormControl(this.materia.creditos, [Validators.required]),
-      'turno': new FormControl(this.materia.turno),
-      'LI': new FormControl(this.materia.LI),
-      'LF': new FormControl(this.materia.LF),
-      'MI': new FormControl(this.materia.MI),
-      'MF': new FormControl(this.materia.MF),
-      'MiI': new FormControl(this.materia.MiI),
-      'MiF': new FormControl(this.materia.MiF),
-      'JI': new FormControl(this.materia.JI),
-      'JF': new FormControl(this.materia.JF),
-      'VI': new FormControl(this.materia.VI),
-      'VF': new FormControl(this.materia.VF),
-      'SI': new FormControl(this.materia.SI),
-      'SF': new FormControl(this.materia.SF),
+      'turno': new FormControl(this.materia.turno)
     });
+  }
+
+  generaStyleTd(){
+    let i = 0;
+    let UltimaPos = 0;
+    for ( i = 0; i < 78; i ++){
+      this.tdStyle[i]='deactivetd';
+      this.tdMateria[i] = {"fila":0,"columna":0};
+    }
+    
+  }
+
+  generaArrayMat(){
+    let o = 0;
+    let a = 0;
+    let Pos =0;
+    for(o=0; o < 13; o++){
+      a=0;
+      for(a = 0; a < 6; a++ ){
+        this.MateriaCorrelacion[Pos]= {"fila": o +1, "columna": a+1};
+        Pos = Pos +1;
+      }
+    }
+    
   }
 
   validaUsuario(){
@@ -64,7 +92,7 @@ export class PrincipalAdminComponent implements OnInit {
   }
 
   public abrirmodal(modal){
-    this.modalService.open(modal,{
+    this.modalRef = this.modalService.open(modal,{
       size: 'lg',
     });
   }
@@ -76,20 +104,9 @@ export class PrincipalAdminComponent implements OnInit {
     this.materia.creditos = null;
     this.materia.departamento = null;
     this.materia.horas = null;
-    this.materia.JF = null;
-    this.materia.JI = null;
-    this.materia.LF = null;
-    this.materia.LI = null;
-    this.materia.MF = null;
-    this.materia.MI = null;
-    this.materia.MiF = null;
-    this.materia.MiI = null;
-    this.materia.VF = null;
-    this.materia.VI = null;
-    this.materia.SF = null;
-    this.materia.SI = null;
       
   }
+  
 
 
 
@@ -100,34 +117,135 @@ export class PrincipalAdminComponent implements OnInit {
   get horas() { return this.materiaForm.get('horas'); }
   get creditos() { return this.materiaForm.get('creditos'); }
   get turno() { return this.materiaForm.get('turno'); }
-  get LI() { return this.materiaForm.get('LI'); }
-  get LF() { return this.materiaForm.get('LF'); }
-  get MI() { return this.materiaForm.get('MI'); }
-  get MF() { return this.materiaForm.get('MF'); }
-  get MiI() { return this.materiaForm.get('MiI'); }
-  get MiF() { return this.materiaForm.get('MiF'); }
-  get JI() { return this.materiaForm.get('JI'); }
-  get JF() { return this.materiaForm.get('JF'); }
-  get VI() { return this.materiaForm.get('VI'); }
-  get VF() { return this.materiaForm.get('VF'); }
-  get SI() { return this.materiaForm.get('SI'); }
-  get SF() { return this.materiaForm.get('SF'); }
 
-  obtieneIdMateria(){
+  obtieneId(tynOp){
       var link = 'http://mauvalsa.com/ControlEscolar/ObtieneDatos.php';
-    var data = JSON.stringify({"opcion": 1});
+    var data = JSON.stringify({"opcion": tynOp});
     this.http.post(link, data)
         .subscribe(respuesta => {
             let res=respuesta.json();
             if(res.success){
-                this.codigoDB = res.response;
+              if (tynOp ==1){
+                this.materia.codigo = res.response;
+                this.generaStyleTd();
+                this.generaArrayMat();
+              }else if(tynOp == 2){
+                this.Alumno.codigo = res.response;
+              }else if(tynOp == 3){
+                this.Maestro.codigo = res.response;
+              }else if(tynOp == 4){
+                this.Aula.codigo = res.response;
+              }
+                
+            }else{
+              if(tynOp == 5){
+                this.AulaMaestro = respuesta.json();
+              }else if(tynOp == 6){
+                this.Aulas = respuesta.json();
+              }else if(tynOp == 8){
+                this.Carreras = respuesta.json();
+              }else{
+              console.log(JSON.stringify(respuesta));
+              }
+            }
+        }, error => {
+            console.log("Oooops!");
+        });
+    
+  }
+  cierraModal(){
+    this.modalRef.dismiss();
+  }
+
+  Registrar(tynOp,value1,value2,value3,value4,value5,value6,value7){
+    var link = 'http://mauvalsa.com/ControlEscolar/Registrar.php';
+    if (tynOp ==1 ){
+      var data = JSON.stringify({"tynOpc": 1,"NumeroAula": value1,"Edificio": value2, "Capacidad": value3 });
+    }else if(tynOp == 2){ 
+      var data = JSON.stringify({"tynOpc": 2,"Nombre": value1,"Telefono": value2, "Domicilio": value3, "IMSS": value4, "CURP": value5, "Departamento": value6 , "codigo": this.Maestro.codigo});
+    }else if(tynOp == 5){ 
+      var data = JSON.stringify({"tynOpc": 5,"Nombre": value1,"Carrera": value2, "Ciclo": value3, "Tel": value4,"domi": value5, "IMSS":value6, "CURP": value7 , "codigo": this.Alumno.codigo});
+    }
+    
+    this.http.post(link, data)
+        .subscribe(respuesta => {
+            let res=respuesta.json();
+            if(res.success){
+              this.modalRef.dismiss();
             }else{
               console.log(JSON.stringify(respuesta));
             }
         }, error => {
             console.log("Oooops!");
         });
+  }
+
+  verificaDisp(aula , maestro){
+    if(aula != "" && maestro != ""){
+      var link = 'http://mauvalsa.com/ControlEscolar/ObtieneDatos.php';
+          var data = JSON.stringify({"opcion": 7,"idAula": aula,"idMaestro": maestro});
+          this.http.post(link, data)
+            .subscribe(respuesta => {
+              let res=respuesta.json();
+              this.generaStyleTd();
+              let arreglo: any[];
+              arreglo = res;
+              let i = 0;
+              let o = 0;
+              for(o = 0; o < arreglo.length; o++){
+                for ( i = 0; i < 78; i ++){
+                  if(this.MateriaCorrelacion[i].fila == arreglo[o].fila && this.MateriaCorrelacion[i].columna == arreglo[o].columna){
+                      this.tdStyle[i]="noDisp";
+                  }
+                }
+              }
+              }, error => {
+                console.log("Oooops!");
+            });
+      this.tablaHorarios = "tablaVisible";
+    }
+
+  }
+  tablaMateria(fila,columna,componente){
+    if (this.tdStyle[componente] == 'deactivetd'){
+      this.tdStyle[componente] = 'activetd';
+      this.tdMateria[componente] = {"fila":fila,"columna":columna};
+    }else if (this.tdStyle[componente] == 'activetd'){
+      this.tdStyle[componente] = 'deactivetd';
+      this.tdMateria[componente] = {"fila":0,"columna":0};
+    }
+  }
+
+  guardaMateria( nombre, carrera, dep, creditos,turno,maestro,aula){
+    let cont = 0;
+    for( cont = 0; cont < 78; cont++){
+      if(this.tdMateria[cont].fila != 0 && this.tdMateria[cont].columna != 0){
+          console.log( this.tdMateria[cont].fila + "," + this.tdMateria[cont].columna);
+          var link = 'http://mauvalsa.com/ControlEscolar/Registrar.php';
+          var data = JSON.stringify({"tynOpc": 4,"idMateria": this.materia.codigo,"fila":this.tdMateria[cont].fila,"columna": this.tdMateria[cont].columna, "maestro": maestro, "aula": aula});
     
+          this.http.post(link, data)
+            .subscribe(respuesta => {
+              }, error => {
+                console.log("Oooops!");
+            });
+      }
+    }
+    var link = 'http://mauvalsa.com/ControlEscolar/Registrar.php';
+    var data = JSON.stringify({"tynOpc": 3,"idMateria": this.materia.codigo,"nombre": nombre, "carrera": carrera, "departamento": dep,
+                                "creditos": creditos, "turno": turno, "maestro": maestro, "aula": aula });
+    
+    this.http.post(link, data)
+        .subscribe(respuesta => {
+            let res=respuesta.json();
+            if(res.success){
+              this.modalRef.dismiss();
+            }else{
+              console.log(JSON.stringify(respuesta));
+            }
+        }, error => {
+            console.log("Oooops!");
+        });
   }
 
 }
